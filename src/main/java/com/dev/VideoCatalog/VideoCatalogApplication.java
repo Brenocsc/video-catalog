413d4cf8bd768dbd725d8a762c6507290ddd1093
brenocostabanqi <breno.costa@banqi.com>
2022-12-26 09:10:00 -0300

    feat: first main method

---
+++ src/main/java/com/dev/VideoCatalog/VideoCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.dev.VideoCatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoCatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/VideoCatalog/VideoCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.VideoCatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
