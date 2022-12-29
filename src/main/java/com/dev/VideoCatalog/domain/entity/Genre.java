288769d1608d845cdab024d88df433a92eb463f9
Brenocsc <breno.csc.2013@hotmail.com.br>
2022-12-29 09:55:31 -0300

    feat: Category related with Genre

--- src/main/java/com/dev/VideoCatalog/domain/entity/Genre.java
+++ src/main/java/com/dev/VideoCatalog/domain/entity/Genre.java
@@ -1,13 +1,16 @@
 package com.dev.VideoCatalog.domain.entity;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 public class Genre extends BaseEntity {
 
   private String name;
+  private List<Category> categories = new ArrayList<>();
 
-  public Genre() {
-  }
+  // public Genre() {
+  // }
 
   public Genre(String name) {
     super.generateUUID();
@@ -17,6 +20,18 @@ public class Genre extends BaseEntity {
   public Genre(UUID id, String name) {
     super.setId(id);
     this.setName(name);
+  }
+
+  public Genre(String name, List<Category> categories) {
+    super.generateUUID();
+    this.setName(name);
+    this.setCategories(categories);
+  }
+
+  public Genre(UUID id, String name, List<Category> categories) {
+    super.setId(id);
+    this.setName(name);
+    this.setCategories(categories);
   }
 
   public String getName() {
@@ -29,5 +44,27 @@ public class Genre extends BaseEntity {
     if (name.length() == 0)
       throw new IllegalArgumentException("");
     this.name = name;
+  }
+
+  public List<Category> getCategories() {
+    return this.categories;
+  }
+
+  public void setCategories(List<Category> categories) {
+    if (categories == null)
+      throw new IllegalArgumentException("");
+    this.categories = categories;
+  }
+
+  public void addCategories(Category category) {
+    if (category == null)
+      throw new IllegalArgumentException("");
+    this.categories.add(category);
+  }
+
+  public void removeCategories(Category category) {
+    if (categories == null)
+      throw new IllegalArgumentException("");
+    this.categories.removeIf(c -> this.categories.contains(category));
   }
 }
