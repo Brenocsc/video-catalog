e15a3d2684c8239d287134b63f60d40e6444eacc
Brenocsc <breno.csc.2013@hotmail.com.br>
2023-01-05 17:33:13 -0300

    feat: create CategoryTests class

---
+++ src/test/java/com/dev/VideoCatalog/domain/entity/CategoryTests.java
@@ -0,0 +1,44 @@
+package com.dev.VideoCatalog.domain.entity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.UUID;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class CategoryTests {
+
+  @Test
+  public void throwIllegalArgumentExceptionWhenNameIsNull() {
+    Exception exception = assertThrows(IllegalArgumentException.class, () -> new Category((String) null));
+    String actualMessage = exception.getMessage();
+
+    assertTrue(actualMessage.contains("name is marked non-null but is null"));
+  }
+
+  @Test
+  public void throwIllegalArgumentExceptionWhenIdIsNull() {
+    assertThrows(IllegalArgumentException.class, () -> new Category((UUID) null));
+  }
+
+  @Test
+  public void throwIllegalArgumentExceptionWhenNameIsBlank() {
+    assertThrows(IllegalArgumentException.class, () -> new Category(""));
+  }
+
+  @Test
+  public void createCategory() {
+    final Category entity = new Category("Category 1");
+    assertNotNull(entity);
+    assertEquals(entity.getName(), "Category 1");
+    assertTrue(entity.isValidUUID(entity.getId().toString()));
+  }
+}
