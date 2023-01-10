3cf7eef6e02ebad1cd68aa8ad291fb39cb0d1f59
Brenocsc <breno.csc.2013@hotmail.com.br>
2023-01-10 16:01:45 -0300

    feat: remove run method from VideoCatalogApplication

--- src/main/java/com/dev/VideoCatalog/VideoCatalogApplication.java
+++ src/main/java/com/dev/VideoCatalog/VideoCatalogApplication.java
@@ -1,25 +1,12 @@
 package com.dev.VideoCatalog;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.dev.VideoCatalog.domain.entity.Category;
-import com.dev.VideoCatalog.domain.entity.Genre;
-
 @SpringBootApplication
-public class VideoCatalogApplication implements CommandLineRunner {
+public class VideoCatalogApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VideoCatalogApplication.class, args);
 	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		Genre genre = new Genre("Genre 1");
-		Category category1 = new Category("Category 1");
-		genre.addCategories(category1);
-		System.out.println("Genre name: " + genre.getName() + genre.getId() + genre.getCategories());
-	}
-
 }
